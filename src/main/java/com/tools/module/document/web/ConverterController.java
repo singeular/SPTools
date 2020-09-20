bc5c42eff7de5511fa25d4f92502c506f5443c09
小柒2012 <345849402@qq.com>
2020-09-20 18:50:01 +0800

    :sparkles: Doc文档在线预览

--- src/main/java/com/tools/module/document/web/ConverterController.java
+++ src/main/java/com/tools/module/document/web/ConverterController.java
@@ -1,11 +1,17 @@
 package com.tools.module.document.web;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import com.tools.common.config.AbstractController;
+import com.tools.common.constant.SystemConstant;
+import com.tools.common.model.Result;
+import com.tools.module.sys.model.SysFile;
+import io.swagger.annotations.Api;
 import org.jodconverter.core.DocumentConverter;
 import org.jodconverter.core.office.OfficeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +19,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.tools.common.config.AbstractController;
-import com.tools.common.constant.SystemConstant;
-import com.tools.common.model.Result;
-import com.tools.module.sys.model.SysFile;
-
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.IdUtil;
-import io.swagger.annotations.Api;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(tags = "文档管理")
 @RestController
 @RequestMapping("document")
 public class ConverterController extends AbstractController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ConverterController.class);
 
     @Value("${file.path}")
     private String filePath;
@@ -39,33 +41,34 @@ public class ConverterController extends AbstractController {
      */
     @RequestMapping("/upload")
     public Result upload(MultipartFile file) {
-	try {
-	    if (file != null) {
-		File parentFile = createParentFile();
-		String fileName = file.getOriginalFilename();
-		String suffix = fileName.substring(fileName.lastIndexOf("."));
-		String uuid = IdUtil.simpleUUID();
-		fileName = uuid + suffix;
-		File imageFile = new File(parentFile, fileName);
-		FileUtil.writeFromStream(file.getInputStream(), imageFile);
-		/**
-		 * 年月日目录
-		 */
-		String fileDay = DateUtil.thisYear() + "/" + (DateUtil.thisMonth() + 1) + "/"
-			+ DateUtil.thisDayOfMonth();
-		String imagePath = SystemConstant.FILE + "/document/" + fileDay + "/" + fileName;
-		/**
-		 * 实时转换
-		 */
-		File toFile = new File(parentFile, uuid + ".pdf");
-		documentConverter.convert(imageFile).to(toFile).execute();
-		return Result.ok(imagePath);
-	    } else {
-		return Result.error();
-	    }
-	} catch (Exception e) {
-	    return Result.error();
-	}
+        try {
+            if (file != null) {
+                File parentFile = createParentFile();
+                String fileName = file.getOriginalFilename();
+                String suffix = fileName.substring(fileName.lastIndexOf("."));
+                String uuid = IdUtil.simpleUUID();
+                fileName = uuid + suffix;
+                File imageFile = new File(parentFile, fileName);
+                FileUtil.writeFromStream(file.getInputStream(), imageFile);
+                /**
+                 * 年月日目录
+                 */
+                String fileDay = DateUtil.thisYear() + "/" + (DateUtil.thisMonth() + 1) + "/"
+                        + DateUtil.thisDayOfMonth();
+                String imagePath = SystemConstant.FILE + "/document/" + fileDay + "/" + fileName;
+                /**
+                 * 实时转换
+                 */
+                File toFile = new File(parentFile, uuid + ".pdf");
+                documentConverter.convert(imageFile).to(toFile).execute();
+                return Result.ok(imagePath);
+            } else {
+                return Result.error();
+            }
+        } catch (Exception e) {
+            LOGGER.error("转换异常{}",e);
+            return Result.error();
+        }
     }
 
     /**
@@ -73,13 +76,13 @@ public class ConverterController extends AbstractController {
      */
     @RequestMapping("converter")
     public Result converter() {
-	try {
-	    documentConverter.convert(new File("F:\\前端.txt")).to(new File("E:\\home\\1.pdf")).execute();
-	} catch (OfficeException e) {
-	    logger.error("转换失败{}", e);
-	    return Result.error();
-	}
-	return Result.ok();
+        try {
+            documentConverter.convert(new File("F:\\前端.txt")).to(new File("E:\\home\\1.pdf")).execute();
+        } catch (OfficeException e) {
+            logger.error("转换失败{}", e);
+            return Result.error();
+        }
+        return Result.ok();
     }
 
     /**
@@ -89,10 +92,10 @@ public class ConverterController extends AbstractController {
      */
     @RequestMapping(value = "list", method = RequestMethod.POST)
     public Result list() {
-	String document = filePath + SystemConstant.SF_FILE_SEPARATOR + "document";
-	List<SysFile> fileList = new ArrayList<>();
-	getAllFilePaths(document, fileList, 0, "");
-	return Result.ok(fileList);
+        String document = filePath + SystemConstant.SF_FILE_SEPARATOR + "document";
+        List<SysFile> fileList = new ArrayList<>();
+        getAllFilePaths(document, fileList, 0, "");
+        return Result.ok(fileList);
     }
 
     /**
@@ -103,40 +106,40 @@ public class ConverterController extends AbstractController {
      * @return
      */
     private static List<SysFile> getAllFilePaths(String filePath, List<SysFile> filePathList, Integer level,
-	    String parentPath) {
-	File[] files = new File(filePath).listFiles();
-	if (files == null) {
-	    return filePathList;
-	}
-	for (File file : files) {
-	    int num = filePathList.size() + 1;
-	    SysFile sysFile = new SysFile();
-	    sysFile.setName(file.getName());
-	    sysFile.setFileId(num);
-	    sysFile.setParentId(level);
-	    if (file.isDirectory()) {
-		sysFile.setDirectory(true);
-		if (level == 0) {
-		    filePathList.add(sysFile);
-		    parentPath = "/" + file.getName();
-		    getAllFilePaths(file.getAbsolutePath(), filePathList, num, parentPath);
-		    num++;
-		} else {
-		    filePathList.add(sysFile);
-		    String subParentPath = parentPath + "/" + file.getName();
-		    getAllFilePaths(file.getAbsolutePath(), filePathList, num, subParentPath);
-		    num++;
-		}
-	    } else {
-		if (level != 0) {
-		    sysFile.setDirectory(false);
-		    sysFile.setParentPath(parentPath + "/" + file.getName());
-		    filePathList.add(sysFile);
-		    num++;
-		}
-	    }
-	}
-	return filePathList;
+                                                 String parentPath) {
+        File[] files = new File(filePath).listFiles();
+        if (files == null) {
+            return filePathList;
+        }
+        for (File file : files) {
+            int num = filePathList.size() + 1;
+            SysFile sysFile = new SysFile();
+            sysFile.setName(file.getName());
+            sysFile.setFileId(num);
+            sysFile.setParentId(level);
+            if (file.isDirectory()) {
+                sysFile.setDirectory(true);
+                if (level == 0) {
+                    filePathList.add(sysFile);
+                    parentPath = "/" + file.getName();
+                    getAllFilePaths(file.getAbsolutePath(), filePathList, num, parentPath);
+                    num++;
+                } else {
+                    filePathList.add(sysFile);
+                    String subParentPath = parentPath + "/" + file.getName();
+                    getAllFilePaths(file.getAbsolutePath(), filePathList, num, subParentPath);
+                    num++;
+                }
+            } else {
+                if (level != 0) {
+                    sysFile.setDirectory(false);
+                    sysFile.setParentPath(parentPath + "/" + file.getName());
+                    filePathList.add(sysFile);
+                    num++;
+                }
+            }
+        }
+        return filePathList;
     }
 
     /**
@@ -145,19 +148,19 @@ public class ConverterController extends AbstractController {
      * @return
      */
     public File createParentFile() {
-	File parentFile = new File(filePath + SystemConstant.SF_FILE_SEPARATOR + "document"
-		+ SystemConstant.SF_FILE_SEPARATOR + DateUtil.thisYear());
-	if (!parentFile.exists()) {
-	    parentFile.mkdirs();
-	}
-	parentFile = new File(parentFile, (DateUtil.thisMonth() + 1) + "");
-	if (!parentFile.exists()) {
-	    parentFile.mkdirs();
-	}
-	parentFile = new File(parentFile, DateUtil.thisDayOfMonth() + "");
-	if (!parentFile.exists()) {
-	    parentFile.mkdirs();
-	}
-	return parentFile;
+        File parentFile = new File(filePath + SystemConstant.SF_FILE_SEPARATOR + "document"
+                + SystemConstant.SF_FILE_SEPARATOR + DateUtil.thisYear());
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        parentFile = new File(parentFile, (DateUtil.thisMonth() + 1) + "");
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        parentFile = new File(parentFile, DateUtil.thisDayOfMonth() + "");
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        return parentFile;
     }
 }
